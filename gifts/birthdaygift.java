fdcfb9c886a918a89f466763e594e00d2e775a67
Karishma Kolhe <karishmakolhe49@gmail.com>
2023-07-19 21:28:51 +0530

    gifting commit

---
+++ gifts/birthdaygift.java
@@ -0,0 +1,14 @@
+package gifts;
+
+public class birthdaygift {
+    public void Display() {
+        System.out.println("Cake");
+    }
+
+    public static void main(String[] args) {
+        birthdaygift bg = new birthdaygift();
+        bg.Display();
+        System.out.println("Happy Birthday");
+    }
+}
+    
